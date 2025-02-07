65c886e15e6e3007046153aef57971ec9a9984e9
Ilsa <irehmat123@gmail.com>
2025-02-07 15:53:33 -0500

    Practice lab 3

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -17,6 +17,51 @@ public class ClockDisplay
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String displayString;    // simulates the actual display
+    /* 
+     * Answer to extra chaper questions
+     * 
+     * 29)
+     * It would take 60 calls to go from 00:00 to 1 min
+     * We can also use set time method to change it to 1 min
+     * 
+     * 30) private Rectangle window;
+     * 
+     *          window = new rectangle(5,4) ;
+     *          
+     * 31)
+     * The second constructor accepts two parameters ( hour and minute) and 
+     * initializes the clock with these values.
+     * It directly sets hours and minutes fields to the provided values
+     * 
+     * 32)
+     * The first constructor sets time to 00:0
+     * The second constructor initializes time with provided hour and minute 
+     * values
+     * updateDisplay() is not called in the second constructor if it directly
+     * sets the displayString without additional processing
+     * 
+     * 33) Printer P1, p2;
+     * 
+     *         p1.print("xxxxx",true)
+     *         
+     *         p2.print("xxxxx", false)
+     *         
+     *         int status = p1.getStatus(3);
+     *         
+     * 34)
+     * The picture class creates objects of Square, Triangle, and Circle.
+     * 
+     * 35)
+     * roof.changeSize(60, 180);
+     * roof.moveHorizontal(20);
+     * roof.moveVertical(-60);
+     * roof.makeVisible();
+     * 
+     * 36)
+     * Yes, the draw() method likely calls other methods within the Picture 
+     * class.
+     *         
+     */
     
     /**
      * Constructor for ClockDisplay objects. This constructor 
@@ -81,4 +126,5 @@ public class ClockDisplay
         displayString = hours.getDisplayValue() + ":" + 
                         minutes.getDisplayValue();
     }
+    
 }
